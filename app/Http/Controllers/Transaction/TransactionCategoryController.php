5294bf7cfc037a2b4927ee466159f18e9010aa8e
Max Alva <alva85@gmail.com>
2019-04-18 13:41:22 -0500

    Implementando las Operaciones Complejas con Transaction

---
+++ app/Http/Controllers/Transaction/TransactionCategoryController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Transaction;
+
+use App\Http\Controllers\ApiController;
+use App\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class TransactionCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Transaction $transaction
+     * @return Response
+     */
+    public function index(Transaction $transaction)
+    {
+        $categories = $transaction->product->categories;
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Transaction $transaction
+     * @return Response
+     */
+    public function show(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Transaction $transaction
+     * @return Response
+     */
+    public function edit(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Transaction $transaction
+     * @return Response
+     */
+    public function update(Request $request, Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Transaction $transaction
+     * @return Response
+     */
+    public function destroy(Transaction $transaction)
+    {
+        //
+    }
+}
